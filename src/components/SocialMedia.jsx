11f6b0924200c1599e8a95cd1c46947bdaf3711e
Youssef-Ahmed <ya23122002@gmail.com>
2022-11-09 03:31:46 +0200

    upload files

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -6,17 +6,29 @@ const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <a href="https://twitter.com/ya23122002">
+        <a
+          href="https://twitter.com/ya23122002"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsTwitter />
         </a>
       </div>
       <div>
-        <a href="https://www.facebook.com/profile.php?id=100011448804193">
+        <a
+          href="https://www.facebook.com/profile.php?id=100011448804193"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaFacebookF />
         </a>
       </div>
       <div>
-        <a href="https://www.instagram.com/youssef1125/">
+        <a
+          href="https://www.instagram.com/youssef1125/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsInstagram />
         </a>
       </div>
